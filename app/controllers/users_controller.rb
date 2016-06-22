773cfdcb2a80d6e04d7bdcc5c1ec452a074764f6
trevormast <trevormast@gmail.com>
2016-06-22 10:30:09 -0700

    Skips csrf verification for users#updategeolocation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
   before_filter :signed_in_user, only: [:edit, :update, :index, :destroy]
   before_filter :correct_user,   only: [:edit, :update, :show]
   before_filter :admin_user,     only: :destroy
+  skip_before_filter :verify_authenticity_token, :only => [:update_geolocation]
 
 	def show
 		@user = User.find(params[:id])
