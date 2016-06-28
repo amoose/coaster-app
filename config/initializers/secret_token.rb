d76e7bb4c8ec49c9fdbdb5c79dce09a337646c64
trevormast <trevormast@gmail.com>
2016-06-28 11:58:31 -0700

    Deletes deprecated secret_token.rb

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Coaster::Application.config.secret_token = 'd1c10ddcb42aaed2072f25f713145814a71f0148096c0499dfe009f36cc012787288ca20b6352e2eed69554253cbb668e12b60e0691cd26fd46cb3b9988d163d'
